1e5caf0f4dc0ab96d420d2892d6a351605aab529
lisetemyyrsepp <116277824+lisetemyyrsepp@users.noreply.github.com>
2024-12-16 07:05:50 +0200

    Post page modifications

--- src/views/AddPostView.vue
+++ src/views/AddPostView.vue
@@ -4,8 +4,8 @@
         <h2>Add Post</h2>
         <form @submit.prevent="addPost">
           <div class="form-group">
-            <label for="body">Body</label>
-            <textarea id="body" v-model="body" placeholder="Enter post content"></textarea>
+            <label for="body">Body       </label>
+            <input id="body" v-model="body" placeholder="body"/>
           </div>
           <button type="submit">Add</button>
         </form>
@@ -22,10 +22,46 @@
     },
     methods: {
       addPost() {
-        // Logic to add post to the database
+        if (this.body.trim() === '') {
+            alert('Body cannot be empty!');
+            return;
+        }
         console.log("Post added:", this.body);
+        this.$router.push({ name: 'MainView' });
       }
     }
   };
   </script>
+
+<style scoped>
+.defaultBox {
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  width: 100%;
+  max-width: 500px;
+}
+
+h2 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.form-group {
+  display: block;
+  flex-direction: row;
+  margin-bottom: 20px;
+}
+
+input {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 10px; 
+  font-size: 16px;
+  outline: none;
+}
+button {
+    width: 20%;
+}
+</style>
   
