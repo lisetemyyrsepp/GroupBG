229dca8dc24164f33603544ce74742d7975e7820
ArianaJogi <arianajogi@gmail.com>
2024-12-16 08:52:40 +0200

    Updated contacts page

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="main">
     <div class="sidebar-left"></div>
-    <div class="content">
-    </div>
+    <div class="defaultBox">
+      <div class="content">
+        <label class="form-label">Email: <span class="info">postit@gmail.com</span></label>
+        <label class="form-label">Phone: <span class="info">56543467</span></label>
+      </div>
+  </div>
     <div class="sidebar-right"></div>
   </div>
 </template>
@@ -14,5 +18,34 @@ export default {
 </script>
 
 <style>
+  body {
+    display: flex;
+    justify-content: center;
+    padding-top: 6em;
+    font-family: Arial, serif;
+    color: var(--text-color);
+    line-height:1.6;
+    margin: 0;
+    background-color: var(--background-color);
+  }
+  
+  .defaultBox {
+    padding: 60px;
+    border-radius: 10px;
+    text-align: center;
+    background-color: var(--box-color);
+    margin-bottom: 20px;
+    position: relative;
+    max-width: 400px;
+  }
 
+  .form-label {
+  display: flex;
+  justify-content: center;
+}
+
+.info {
+  margin-left: 5px;
+  font-weight: bold;
+  }
 </style>
